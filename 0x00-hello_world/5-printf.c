65090fa4e5eca8af32f78f3990107750d21d80cd
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 05:47:53 -0700

    my printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/** 
+/**
 * main - print the string in the printf function
 * Description: using the main function
 * this program prints "with proper grammar, but the outcome is a piece of art"
-* Return: 0
+* Return:0
 */
 int main(void)
 {
