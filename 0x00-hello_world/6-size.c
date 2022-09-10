2cf985d53b6171592f9766c23c284f0fb0fc2d76
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 06:50:05 -0700

    My size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void)
+/**
+* main - prints the string in the printf function
+* Description: using the main function
+* this program prints sizes of the various data types
+* return: 0
+*/
+{
+int i;
+char c;
+long int li;
+long long int lli;
+float f;
+printf("The size of an int is: %lu.\n", (unsigned long) Sizeof(i));
+printf("the size of an c is: %lu.\n", (unsigned long) Sizeof(c));
+printf("The size of an li is: %lu.\n", (unsigned long) Sizeof(li));
+printf("The size of an lli is: %lu.\n" (unsigned long) Sizeof(lli));
+printf("The size of an f is: %lu.\n" (unsigned long)Sizeof(f));
+return (0)
+}
