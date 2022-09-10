36090d8321be870db2da36357da0016b405ec422
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 02:55:51 -0700

    my c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - return nothing since no argument
+ *
+ * description using the main function
+ * this program prints "programming is like a multilingual puzzle
+ * return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
