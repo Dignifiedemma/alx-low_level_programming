bb01b62894bbf1eb669111e58fd00a6ef8686466
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 07:20:07 -0700

    My size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(void)
 /**
+* main - enrty points
 * main - prints the string in the printf function
 * Description: using the main function
 * this program prints sizes of the various data types
@@ -12,10 +13,10 @@ char c;
 long int li;
 long long int lli;
 float f;
-printf("The size of an int is: %d.\n", sizeof(i));
-printf("the size of a char is: %d.\n", sizeof(c));
-printf("The size of a long int is: %d.\n", sizeof(li));
-printf("The size of a long long int is: %d.\n", sizeof(lli));
-printf("The size of a float is: %d.\n", sizeof(f));
+printf("The size of an int is: %d byte(s).\n", sizeof(i));
+printf("the size of a char is: %d byte(s).\n", sizeof(c));
+printf("The size of a long int is: %d byte(s).\n", sizeof(li));
+printf("The size of a long long int is: %d byte(s).\n", sizeof(lli));
+printf("The size of a float is: %d byte(s)\n", sizeof(f));
 return (0);
 }
