b0018957d984c404e30a7d08a4f4c55b2d53417c
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 03:34:48 -0700

    my c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,7 @@
  * this program prints \"programming is like building a multilingual puzzle
  */
 int main()
-	//string initialization
- char hello_puts = "Programming is like building a multilingual puzzle
 {
-	puts("hello_puts\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
