08aafab11a0084124fd564c350008b11e7b75484
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 02:26:44 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
-char hello_put[] = "Programming is like building a multilingual puzzle";
-puts(hello_put);
-return 0;
+puts(""Programming is like building a multilingual puzzle\n");
+return (0);
 }
