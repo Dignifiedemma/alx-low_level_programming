fa9f0f004324ddd50f0e6d2cec000c0e6534dd21
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 05:03:36 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-/* function begins program execution
-*/
+/* function begins program execution*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+/** printf prints with proper grammar,but the outcome is a piece of art
+ * return 0
+*/
+printf("with proper grammar,but the outcome is a piece of art,\n");
 return (0);
 }
