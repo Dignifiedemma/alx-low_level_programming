4d81662eadd1b699fd2d076fad318f6fd5358bc8
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 03:37:36 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Description : using the main function
  * this program prints \"programming is like building a multilingual puzzle
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
