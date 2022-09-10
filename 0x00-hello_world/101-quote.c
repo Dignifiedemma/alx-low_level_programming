718af2888b7b57335d4cfcd365584f0dc76e4ae1
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 11:19:12 -0700

    My commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
  * main - Entry point
  * Description: using the main function
  * this program prints the string
- * return: 1
+ * return: Always 1[success]
  */
-int main()
+int main(void)
 {
 char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, str1, 59);
