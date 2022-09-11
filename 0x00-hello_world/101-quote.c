1601b1d58796962fea116d34439dbf8666b3de22
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-11 07:44:19 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * return: always return 1 (success)
- */
+* main - print the string in the write fuction
+*
+* Description: using the main function
+* this program prints and that piece of art is useful\" - Dora Korpar...
+* Return: 1
+*/
 int main(void)
 {
 write(2,
