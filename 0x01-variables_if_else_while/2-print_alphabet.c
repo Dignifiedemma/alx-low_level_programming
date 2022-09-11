de4ba72bbe024d91d3addd7b772556cb47fe531a
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-11 02:42:32 -0700

    My alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print the alphabet
+ * Description:using the main function
+ * this program prints out the alphabet
+ * return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch ='a';ch <= 'z'; ch++);
+putchar(ch);
+putchar('\n');
+return (0);
+}
