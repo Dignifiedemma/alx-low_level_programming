785e6c63eea118ccd664b8127e85ff38c7080fa8
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-11 06:41:33 -0700

    my commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - print the lower case alphabet of ch
- * Description: using the main function
- * this program prints out the alphabet in lower cases
- * return: 0
+ * main - entry point
+ *
+ * Return: always return 0
+ *
  */
 int main(void)
 {
