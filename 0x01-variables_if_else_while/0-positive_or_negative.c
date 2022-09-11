9e5608417cfe04537d24825487b3226ca5d1641f
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-10 23:55:49 -0700

    my if commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/** main - prints if number is positive, zero or negative
- * return: 0  
+/** main - prints the out put of the string
+ * Description: using the main function
+ * thi program prints if (number stored in the variable is positive or negative)
+ * return: 0
  */
 int main(void)
 {
@@ -13,7 +15,7 @@ if (n > 0)
 	printf("%d is positive\n", n);
 else if (n < 0)
 	printf("%d is negative\n", n);
-else 
+else
 	printf("%d is zero\n", n);
 return (0);
 }
