4cbd1a9f1fa1b7d9db3fae70b1c5146ca7d72d19
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-13 12:25:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{ 
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
