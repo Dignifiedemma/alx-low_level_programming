62f8bbac21b763394a901476e9480b632e4d26de
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-14 11:56:34 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 {
 int row, column, product;
 
-if (n >= 0 && n < 15)
+if (n >= 0 && n <= 14)
 {
 for (row = 0; row <= n; row++)
 {
@@ -19,6 +19,7 @@ for (column = 0; column <= n; column++)
 product = row * column;
 
 if (column == 0)
+}
 _putchar('0');
 else if (product < 10)
 {
@@ -46,6 +47,5 @@ else if (product > 99 && product < 100)
 	}
 }
 _putchar('\n');
-}
 }
 }
