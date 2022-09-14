d5b73fbf0de654f213e004f8a891187c26e940f7
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-14 10:42:28 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*
+* print_times_table - Prints a multiplication table up to param
+* @n: The number to be treated
+*
+* Return: Number matrix
+*/
+void print_times_table(int n)
+
+{
+	int num, prod, mult;
+	for (num = 0; num <= 0; num++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 0; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+				if (prod <= 15)
+					_putchar(' ');
+				else
+					_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
