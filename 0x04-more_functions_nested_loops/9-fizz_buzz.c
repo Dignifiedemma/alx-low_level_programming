4644df2e963451e97a4697498dfbe32096a0a0f5
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-16 10:29:52 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,14 @@
  */
 
 int main(void)
-
 {
 
-		int n = 1;
+int n = 1;
 
-
-
-			while (n < 100)
-
-					{
-
-								if (n % 15 == 0)
-
-												printf("FizzBuzz ");
-
+while (n < 100)
+{
+if (n % 15 == 0)
+printf("FizzBuzz ");
 else if (n % 5 == 0)
 printf("Buzz ");
 else if (n % 3 == 0)
