147e167b33908587441db61b47edc6d993782e5c
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-16 11:06:09 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,53 +9,26 @@
 void print_triangle(int size)
 
 {
-
-		int col = 0;
-
-			int row = 0;
-
-				int t;
-
-
-
-					if (size > 0)
-
-							{
-
-										while (row < size)
-
-													{
-
-																	t = size - row - 1;
-
-																				while (col < size)
-
-																								{
-
-																													if (col < t)
-
-																																			_putchar(32);
-
-																																	else
-
-																																							_putchar(35);
-
-																																					col++;
-
-																																								}
-
-																							_putchar(10);
-
-																										col = 0;
-
-																													row++;
-
-																															}
-
-											}
-
-						else
-
-									_putchar(10);
-
+	int col = 0;
+	int row = 0;
+	int t;
+	if (size > 0)
+	{
+		while (row < size)
+		{
+			t = size - row - 1;
+			while (col < size)
+			{
+				if (col < t)
+					_putchar(32);
+				else
+_putchar(35);
+col++
+	_putchar(10
+			col = 0;
+			row++;
+			}
+			}	
+else
+_putchar(10);
 }
