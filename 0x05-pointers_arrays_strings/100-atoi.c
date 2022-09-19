db9df925a779330b37d4983515ccd9af7e32470f
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-19 16:45:41 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,17 +20,22 @@ int _atoi(char *s)
 	if (s[i] == '-')
 p *= -1;
 
-	if (s[i] > 47 && s[i] < 58)
+if (s[i] > 47 && s[i] < 58)
 {
 if (h < 0)
-	h = (h * 10) - (s[i] - '0');
+
+h = (h * 10) - (s[i] - '0');
 else
-h = (s[i] - '0') * -1;
+
+h = ((s[i] - '0') * -1);
+
 if (s[i + 1] < 48 || s[i + 1] > 57)
 
 	break;
 }
 }
 if (p < 0)
+
 h *= -1;
-return (h);}
+return (h);
+}
