3a537977e49f6d20d400112172f4ccd217cc604f
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-19 16:23:08 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function
+ * return: zero
+ */
+int main(void)
+{
+intum;
+
+char c;
+
+srand(time(NULL));
+
+while (sum <= 2645)
+
+	c = rand() % 128;
+
+sum += c;
+
+putchar(c);
+
+putchar(2772 -um);
+
+return (0);
+}
