9c50afa98c6bdf21df7a6bf9d5b386e88cf71448
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-20 07:20:07 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,8 +2,7 @@
 /**
  * _strlen - function that returns the length of a string
  * @s: pinter parameter
- *  _strlen - returns the number of bytes in
- * the string pointed to bt s
+ *  Return: length of the string
  */
 int _strlen(char *s)
 
