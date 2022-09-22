f3249a460f516f939117e6bcd4e186bafb28178e
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-22 13:44:54 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,23 +7,20 @@
 char *cap_string(char *s)
 {
 	int i = 0, j;
-	char a[] = "\t\n , ; . ! ? \ " ( ){ }";
+	char special[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
 	while (*(s + i))
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		for (j = 0; j < 13; j++)
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
+			if (i == 0 && s[i] >= 97 && s[i] <= 122)
+				s[i] -= 32;
+			if (s[i - 1] == special[j])
 			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
+				if ((*(s + i) >= 97) && (*(s + i) <= 122))
+					*(s + i) -= 32;
 				}
 			}
-		}
 		i++;
 	}
 	return (s);
