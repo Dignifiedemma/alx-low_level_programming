b748c912b2ff1263dfba094451d43c161e0f91ad
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-27 10:20:52 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-/* main - making magic */
 int main(void)
 {
 int n;
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * infinite_add - c function that adds two numbers stored
