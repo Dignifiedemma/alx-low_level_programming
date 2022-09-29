891e7aa13bf68659918f915a877f12445817c3aa
Dignifiedemma <ajahemmanuel049@gmail.com>
2022-09-29 13:21:21 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -27,11 +27,10 @@ int is_prime_number(int n)
 {
 	int start = n / 2;
 	
-	start++;
-
 	if (start <= 1)
-		return (1);
-	else if (n % start == 0)
 		return (0);
+	else if (n % start == 0)
+		start++;
+		return (1);
 	return (is_prime(n, start - 1));
 }
